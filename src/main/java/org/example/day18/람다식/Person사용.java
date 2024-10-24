0d1f38de017f27fa0cb6d313825d5816110a4f8b
gunny <gunny97h@gmail.com>
2024-10-24 14:35:30 +0900

    24.10.24 수업 함수형 인터페이스 람다

--- src/main/java/org/example/day18/람다식/Person사용.java
+++ src/main/java/org/example/day18/람다식/Person사용.java
@@ -18,12 +18,12 @@ public class Person사용 {
 
         // 처리함수 참고
         // 정적(static) 메서드
-        person.action2(x -> 처리함수.staticPrint(x), "람다식");
-        person.action2(처리함수::staticPrint, ":: 사용");
+        person.action2(x -> 처리함수.staticPrint(x), "람다 표현식");
+        person.action2(처리함수::staticPrint, "메서드 참조(:: 사용)");
 
         // 일반(instance) 메서드
         처리함수 p = new 처리함수();
-        person.action1((x, y) -> p.generalPrint(x, y), "임아무개", "프로그래머");
-        person.action1(p::generalPrint, "임아무개", "프로그래머");
+        person.action1((x, y) -> p.generalPrint(x, y), "임아무개", "프로그래머");  // 람다 표현식
+        person.action1(p::generalPrint, "임아무개", "프로그래머");  // 메서드 참조
     }
 }
