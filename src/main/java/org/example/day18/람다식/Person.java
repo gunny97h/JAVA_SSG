7873a5eb851b9717683d511acca6e0c096ee3352
gunny <gunny97h@gmail.com>
2024-10-24 14:20:31 +0900

    24.10.24 수업 함수형 인터페이스 람다

--- src/main/java/org/example/day18/람다식/Person.java
+++ src/main/java/org/example/day18/람다식/Person.java
@@ -1,11 +1,11 @@
 package org.example.day18.람다식;
 
 public class Person {
-    public void action1(Workable w) {
-        w.work("홍길동", "프로그래머");
+    public void action1(Workable w, String name, String job) {
+        w.work(name, job);
     }
 
-    public void action2(Speakable s) {
-        s.speak("안녕하세요.");
+    public void action2(Speakable s, String content) {
+        s.speak(content);
     }
 }
--- src/main/java/org/example/day18/람다식/Person사용.java
+++ src/main/java/org/example/day18/람다식/Person사용.java
@@ -2,15 +2,18 @@ package org.example.day18.람다식;
 
 public class Person사용 {
     public static void main(String[] args) {
+
         Person person = new Person();
 
         person.action1((x, y) -> {
-            System.out.println(x + "님은 ");
+            System.out.println(x + "님은 ");  // <-- Workable w
             System.out.println(y + "입니다.");
-        });
+        }, "임아무개", "프로그래머");
 
-        person.action1((x, y) -> System.out.println(x + "님은 " + y + "입니다."));
+        // -->
+        // person.action1((x, y) -> Workable w, String name, String job);
+        person.action1((x, y) -> System.out.println(x + "님은 " + y + "입니다."), "", "");
 
-        person.action2(x -> System.out.println(x + "가 내용입니다."));
+        person.action2(x -> System.out.println(x + "가 내용입니다."), "abc"); // (Speakable s, String content)
     }
 }
