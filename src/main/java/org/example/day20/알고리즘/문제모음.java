e4c3c732b4324a9b72d0a97d5f4e08b8679d407e
gunny <gunny97h@gmail.com>
2024-10-28 18:04:56 +0900

    24.10.28 문제 - 알고리즘

--- src/main/java/org/example/day20/알고리즘/문제모음.java
+++ src/main/java/org/example/day20/알고리즘/문제모음.java
@@ -120,6 +120,13 @@ public class 문제모음 {
         //    n명이 주문한 피자를 남기지 않고 모두 같은 수의 피자 조각을 먹어야 한다면 최소 몇 판을 시켜야 하는지를
         //    return 하도록 solution 함수를 완성해보세요.
 
+        int n10 = 10;
+        int plates = 1;
+        while ((plates * 6) % n10 != 0) {
+            plates++;
+        }
+        System.out.println(plates);
+
 
 
     }
